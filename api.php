7a2f5e4abbe0f3f9f12aae85d3790241827bac3a
Hector <hectorumaga@hotmail.com>
2017-05-18 10:04:31 -0300

    mysqli

--- api.php
+++ api.php
@@ -1,24 +1,24 @@
 <?php
 	
-$db = mysql_connect("app_semana.mysql.dbaas.com.br", "app_semana", "admin_app") or die("Nao foi possivel conectar ao servidos: ".mysql_error());
-mysql_select_db("app_semana") or die("Nao foi possivel localizar banco de dados: ".mysql_error());
-mysql_set_charset("utf8");
+$db = mysqli_connect("app_semana.mysql.dbaas.com.br", "app_semana", "admin_app") or die("Nao foi possivel conectar ao servidos: ".mysqli_error());
+mysqli_select_db("app_semana") or die("Nao foi possivel localizar banco de dados: ".mysqli_error());
+mysqli_set_charset("utf8");
 $table = isset($_GET["table"]) ? $_GET["table"] : "";
 $periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
 if($table == "curso"){
-    $result = mysql_query("SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysql_error());
+    $result = mysqli_query("SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error());
 }
 else{
-    $result = mysql_query("SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysql_error());
+    $result = mysqli_query("SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error());
 }
-if (mysql_num_rows($result) == 0) {
+if (mysqli_num_rows($result) == 0) {
     echo ("Nenhuma linha foi achada");
     exit;
 }
 $print = array();
-while($row = mysql_fetch_assoc($result)){
+while($row = mysqli_fetch_assoc($result)){
     $print[] = $row;
 }
 echo json_encode($print, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-mysql_close($db);
+mysqli_close($db);
 ?>
