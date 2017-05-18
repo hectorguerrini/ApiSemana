d238afece9ca9675375ec61e28976a85fb5dd52a
Hector <hectorumaga@hotmail.com>
2017-05-18 10:14:16 -0300

    mysqli1

--- api.php
+++ api.php
@@ -1,15 +1,15 @@
 <?php
 	
-$db = mysqli_connect("app_semana.mysql.dbaas.com.br", "app_semana", "admin_app") or die("Nao foi possivel conectar ao servidos: ".mysqli_error());
-mysqli_select_db("app_semana") or die("Nao foi possivel localizar banco de dados: ".mysqli_error());
+$db = mysqli_connect("app_semana.mysql.dbaas.com.br", "app_semana", "admin_app") or die("Nao foi possivel conectar ao servidos: ");
+mysqli_select_db("app_semana") or die("Nao foi possivel localizar banco de dados: ");
 mysqli_set_charset("utf8");
 $table = isset($_GET["table"]) ? $_GET["table"] : "";
 $periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
 if($table == "curso"){
-    $result = mysqli_query("SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error());
+    $result = mysqli_query("SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ");
 }
 else{
-    $result = mysqli_query("SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error());
+    $result = mysqli_query("SELECT * FROM $table") or die("Nao foi possivel realizar query: ");
 }
 if (mysqli_num_rows($result) == 0) {
     echo ("Nenhuma linha foi achada");
