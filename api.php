33cf982c070029609263d9fdd9c68af001315229
Hector <hectorumaga@hotmail.com>
2017-05-25 13:19:22 -0300

    Api de cadastro no api.php

--- api.php
+++ api.php
@@ -8,6 +8,12 @@ $periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
 $state = isset($_GET["state"]) ? $_GET["state"] : "";
 if($table == "curso"){
     $result = mysqli_query($db,"SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+}elseif($table == "participante"){
+    $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
+    $email = isset($_GET["email"]) ? $_GET["email"] : "";
+    $pw = isset($_GET["pw"]) ? $_GET["pw"] : "";
+    $cpf = isset($_GET["cpf"]) ? $_GET["cpf"] : "";
+    $result = mysqli_query($db,"INSERT INTO participante (nome_participante,email_participante, password_participante,cpf_participante) VALUES ('$nome','$email','$pw','$cpf')") or die("Nao foi possivel realizar query: ".mysqli_error($db));
 }
 else{
     $result = mysqli_query($db,"SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error($db));
@@ -24,3 +30,6 @@ while($row = mysqli_fetch_assoc($result)){
 echo json_encode($print, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 mysqli_close($db);
 ?>
+
+
+
