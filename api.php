03601ff6c5a1fe640b0c564955ddb6728b7ee2ee
Hector <hectorumaga@hotmail.com>
2017-05-25 15:54:25 -0300

    Api de cadastro no api.php com switch

--- api.php
+++ api.php
@@ -4,10 +4,57 @@ $db = mysqli_connect("app_semana.mysql.dbaas.com.br", "app_semana", "admin_app")
 mysqli_select_db($db,"app_semana") or die("Nao foi possivel localizar banco de dados: ".mysqli_connect_error());
 mysqli_set_charset($db,"utf8");
 $table = isset($_GET["table"]) ? $_GET["table"] : "";
-$periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
-$state = isset($_GET["state"]) ? $_GET["state"] : "";
-if($table == "curso"){
-    $result = mysqli_query($db,"SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+
+switch($table){
+    case "curso":
+	$periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
+	$result = mysqli_query($db,"SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+	if (mysqli_num_rows($result) == 0) {
+	    echo ("Nenhuma linha foi achada");
+    	    exit;
+	}
+	$print = array();
+	while($row = mysqli_fetch_assoc($result)){
+    	    $print[] = $row;
+	}	
+	break;
+    case "participante":
+	$ac = isset($_GET["ac"]) ? $_GET["ac"] : "";
+	if($ac == "cd"){
+	
+	    $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
+            $email = isset($_GET["email"]) ? $_GET["email"] : "";
+            $pw = isset($_GET["pw"]) ? $_GET["pw"] : "";
+            $cpf = isset($_GET["cpf"]) ? $_GET["cpf"] : "";
+            $result = mysqli_query($db,"INSERT INTO participante (nome_participante,email_participante,
+password_participante,cpf_participante) VALUES ('$nome','$email','$pw','$cpf')") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+	    if ($result == 1) {
+    		$print["success"] = "Conta criada com sucesso ".$email;
+	    }else{
+    		$print["error"] = "error";
+	    }
+	}
+	if($ac == "login"){
+	    $email = isset($_GET["email"]) ? $_GET["email"] : "";
+            $pw = isset($_GET["pw"]) ? $_GET["pw"] : "";
+	    
+	}
+	break;
+    default:
+	$result = mysqli_query($db,"SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+	if (mysqli_num_rows($result) == 0) {
+    	    echo ("Nenhuma linha foi achada");
+            exit;
+	}
+	$print = array();
+	while($row = mysqli_fetch_assoc($result)){
+            $print[] = $row;
+	}
+	break;
+}
+
+/*if($table == "curso"){
+    
 }elseif($table == "participante"){
     $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
     $email = isset($_GET["email"]) ? $_GET["email"] : "";
@@ -26,7 +73,7 @@ if (mysqli_num_rows($result) == 0) {
 $print = array();
 while($row = mysqli_fetch_assoc($result)){
     $print[] = $row;
-}
+}*/
 echo json_encode($print, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 mysqli_close($db);
 ?>
