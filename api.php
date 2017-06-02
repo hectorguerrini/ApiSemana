6d70c51d19606e682a3fd41ce8c4a61870e1d1c5
HectorGuerrini <hectorumaga@hotmail.com>
2017-06-02 15:01:20 -0300

    teste chamada da api

--- api.php
+++ api.php
@@ -3,61 +3,7 @@
 require_once('sql_functions.php');
 $db = new sql_functions();
  
-$table = isset($_GET["table"]) ? $_GET["table"] : "";
-
-switch($table){
-    case "curso":
-	$periodo = isset($_GET["periodo"]) ? $_GET["periodo"] : "";
-	$result = mysqli_query($db,"SELECT * FROM curso WHERE periodo_curso = '$periodo'") or die("Nao foi possivel realizar query: ".mysqli_error($db));
-	if (mysqli_num_rows($result) == 0) {
-	    echo ("Nenhuma linha foi achada");
-    	    exit;
-	}
-	$print = array();
-	while($row = mysqli_fetch_assoc($result)){
-    	    $print[] = $row;
-	}	
-	break;
-    case "participante":
-	$ac = isset($_GET["ac"]) ? $_GET["ac"] : "";
-	if($ac == "cadastro"){
-	
-	    $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
-            $email = isset($_GET["email"]) ? $_GET["email"] : "";
-            $pw = isset($_GET["pw"]) ? $_GET["pw"] : "";
-            $cpf = isset($_GET["cpf"]) ? $_GET["cpf"] : "";
-            $result = mysqli_query($db,"INSERT INTO participante (nome_participante,email_participante,
-password_participante,cpf_participante) VALUES ('$nome','$email','$pw','$cpf')") or die("Nao foi possivel realizar query: ".mysqli_error($db));
-	    if ($result == 1) {
-    		$print["retorno"] = "Conta criada com sucesso ".$email;
-	    }else{
-    		$print["retorno"] = "error";
-	    }
-	}
-	if($ac == "login"){
-	    $email = isset($_GET["email"]) ? $_GET["email"] : "";
-            $pw = isset($_GET["pw"]) ? $_GET["pw"] : "";
-	    $result = mysqli_query($db,"SELECT * FROM participante WHERE email_participante = '$email' and password_participante = '$pw' ") or die("Nao foi possivel realizar query: ".mysqli_error($db));
-	    if (mysqli_num_rows($result) > 0) {
-    	        $print["retorno"] = "Bem Vindo ".$email;
-	    }else{
-		$print["retorno"] = "erro";
-	    }
-	
-	}
-	break;
-    default:
-	$result = mysqli_query($db,"SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error($db));
-	if (mysqli_num_rows($result) == 0) {
-    	    echo ("Nenhuma linha foi achada");
-            exit;
-	}
-	$print = array();
-	while($row = mysqli_fetch_assoc($result)){
-            $print[] = $row;
-	}
-	break;
-}
+//$table =  ? $_GET["table"] : "";
 
 /*if($table == "curso"){
     
@@ -68,11 +14,16 @@ password_participante,cpf_participante) VALUES ('$nome','$email','$pw','$cpf')")
     $cpf = isset($_GET["cpf"]) ? $_GET["cpf"] : "";
     $result = mysqli_query($db,"INSERT INTO participante (nome_participante,email_participante, password_participante,cpf_participante) VALUES ('$nome','$email','$pw','$cpf')") or die("Nao foi possivel realizar query: ".mysqli_error($db));
 }
-else{
-    $result = mysqli_query($db,"SELECT * FROM $table") or die("Nao foi possivel realizar query: ".mysqli_error($db));
+*/
+if(isset($_GET["table"]))
+	$table = isset($_GET["table"]);
+
+	$resposta = $db->listarTabelas($table);
+	echo json_encode($resposta, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 }
 
-if (mysqli_num_rows($result) == 0) {
+
+/*if (mysqli_num_rows($result) == 0) {
     echo ("Nenhuma linha foi achada");
     exit;
 }
@@ -81,7 +32,6 @@ while($row = mysqli_fetch_assoc($result)){
     $print[] = $row;
 }*/
 echo json_encode($print, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-mysqli_close($db);
 ?>
 
 
