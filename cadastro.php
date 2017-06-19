44dc2506ec7724fd531529691c0f0d5f0214ed22
HectorGuerrini <hectorumaga@hotmail.com>
2017-06-18 22:48:26 -0300

    validar cpf

--- cadastro.php
+++ cadastro.php
@@ -25,21 +25,27 @@ if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['password'])
     } else {
         if(strlen($password) >=4 && strlen($password) <=16){
             if($db->validarEmail($email) == true){
-                $user = $db->cadastrarUser($nome, $email, $password,$cpf,$rg,$data,$sexo,$tel,$cel);
+                if(verificarCPF($cpf)== true){
+                    $user = $db->cadastrarUser($nome, $email, $password,$cpf,$rg,$data,$sexo,$tel,$cel);
                 
-                if ($user) {
+                    if ($user) {
         
-                    $resposta["error"] = FALSE;
-                    $resposta["user"]["nome"] = $user["nome_participante"];
-                    $resposta["user"]["email"] = $user["email_participante"];
-                    if($db->adcionarPontos($email,10) == true){
+                        $resposta["error"] = FALSE;
+                        $resposta["user"]["nome"] = $user["nome_participante"];
+                        $resposta["user"]["email"] = $user["email_participante"];
+                        if($db->adcionarPontos($email,10) == true){
                         $resposta["user"]["pontos"] = "10";
-                    }   
-                    echo json_encode($resposta);
-                } else {
+                        }   
+                        echo json_encode($resposta);
+                    } else {
                     
+                        $resposta["error"] = TRUE;
+                        $resposta["error_msg"] = "Erro desconhecido ocorreu!";
+                        echo json_encode($resposta);
+                    }
+                }else{
                     $resposta["error"] = TRUE;
-                    $resposta["error_msg"] = "Erro desconhecido ocorreu!";
+                    $resposta["error_msg"] = "CPF invalido!!";
                     echo json_encode($resposta);
                 }
             }else{
@@ -59,4 +65,46 @@ if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['password'])
     echo json_encode($resposta);
 }
 
+
+function verificarCPF( $cpf ) { 
+    
+    $cpf= str_replace("-", "", $cpf);
+    $cpf = str_replace(".", "", $cpf); 
+    
+    if ( strlen( $cpf ) != 11 ) {
+        return false; 
+    } 
+    
+    $codigo = substr($cpf, 0, 9);
+    
+    $soma = 0;
+    $numero = 10; 
+    for ($i=0; $i < 9; $i++) {
+         $soma += ( $codigo[$i]*$numero-- ); 
+    } 
+    $resto = $soma%11;
+    if($resto < 2) {
+        $codigo .= "0"
+    }else{
+        $codigo .= (11-$resto);
+    }
+    
+    $soma = 0;
+    $numero = 11;
+    for ($i=0; $i < 10; $i++) { 
+        $soma += ( $codigo[$i]*$numero-- ); 
+    } 
+    $resto = $soma%11;
+    if($resto < 2){
+        $codigo .= "0"; 
+    }else{ 
+        $codigo .= (11-$resto);
+    }
+    if ( $codigo === $cpf ) { 
+        return true;
+    } else { 
+         return false;
+    } 
+} 
+       
 ?>
